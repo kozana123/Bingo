d5c25720cb753b0335d3722c223bc16db55bf511
shanylus <shanilos100@gmail.com>
2024-07-16 17:47:24 +0300

    שינוי

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,11 @@ import { toggleClass, createBoard, drawNumber } from './functions.js';
 פונקציית חץ
 (כאן יהיו הפרמטרים שהפונקציה מקבלת) => {גוף הפונקציה}
 */
-document.querySelector('#btn1').addEventListener('click', () => {
-  alert(`הכל טוב???`);
-});
+// document.querySelector('#btn1').addEventListener('click', () => {
+//   alert(`הכל טוב???`);
+// });
 
-document.querySelector('#btn2').addEventListener('click', toggleClass);
+// document.querySelector('#btn2').addEventListener('click', toggleClass);
 
 document.querySelector('#start').addEventListener('click', createBoard);
 
